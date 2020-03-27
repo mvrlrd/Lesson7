899f5d205a3c5a1e3b9210b947d3d242e185e1cb
mvrlrd <igrbelykh@gmail.com>
2020-03-27 23:13:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/mvrlrd/test/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.mvrlrd.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Создать пакет unit.
+// Создать метод сложения двух чисел.
+// Протестировать этот метод.
+// Положительный и отрицательный сценарий
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
